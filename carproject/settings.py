033338c1b354b2208261e109a67676e8579f54b3
vighnesh <vighneshrane104@gmail.com>
2024-03-17 08:03:01 +0530

    okay

--- carproject/settings.py
+++ carproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -96,10 +97,15 @@ WSGI_APPLICATION = 'carproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'fC3-ga-3Ce64GfdbdAfGA4bDFdGefg33',
+        'HOST': 'roundhouse.proxy.rlwy.net',
+        'PORT': '58405',
     }
 }
+
 
 
 # Password validation
@@ -136,10 +142,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Static files (CSS, JavaScript, images, fonts)
 STATIC_URL = 'static/'
 
-NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
-# NPM_BIN_PATH = "D:/node/npm.cmd"
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
